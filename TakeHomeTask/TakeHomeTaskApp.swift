23d75b44d7067ace6af897f8094624c2191d565c
garynaz <garynaz@icloud.com>
2021-12-21 18:40:05 -0500

    Initial Commit

---
+++ TakeHomeTask/TakeHomeTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHomeTaskApp.swift
+//  TakeHomeTask
+//
+//  Created by Gary Naz on 12/21/21.
+//
+
+import SwiftUI
+
+@main
+struct TakeHomeTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
